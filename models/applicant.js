034eb82b8950fe761911187a707f5f2e800061e8
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-05-09 14:51:38 +0530

    ok

--- models/applicant.js
+++ models/applicant.js
@@ -2,48 +2,59 @@ const mongoose = require("mongoose")
 const applicantSchema = mongoose.Schema({
     name: {
         type: String,
+        trim:true,
         require: [true, "name is required"]
     },
     email: {
         type: String,
+        trim:true,
         require: [true, "email is required"]
     },
     mobile: { 
         type: Number,
+        trim:true,
         require: [true, "mobile number is required"]
     },  
     role: {
         type: String,
+        trim:true,
         default: "MERN Stack Developer",
         require: [true, "role is required"]
     },
     collegeName: {
         type: String,
+        trim:true,
         require: [true, "college name is required"]
     },
     qualification: {
         type: String,
+        trim:true,
         require: [true, "qualification is required"]
     },
     branch:{
         type:String,
+        trim:true,
         default:"computer science",
         require:[true,"Branch is required"]
     },
     passout: {
         type: Number,
+        trim:true,
         require: [true, 'passout is required']
     },
     nextRound: {
         type: String,
+        trim:true,
         default: 'Bhavya'
     },
     status: {
         type: String,
+        trim:true,
         default: "New"
     },
     experience: {
         type: Number,
+        trim:true,
         default:0
     },
     previousCompany: {
@@ -52,6 +63,7 @@ const applicantSchema = mongoose.Schema({
     },
     resumeLink: {
         type: String,
+        trim:true,
         require: [true, "Resume link is required"]
     },
     comments: [{
