b1c511ab44d05c232fee101c360d870166e02cca
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-05-11 18:06:39 +0530

    ok

--- controllers/sendMails.js
+++ controllers/sendMails.js
@@ -1,29 +1,46 @@
-const nodemailer=require("nodemailer")
-const express=require("express")
-const sendMails=express.Router()
-const transporter=nodemailer.createTransport({
-    service:"gmail",
-    auth:{
-        user:"tirumalarowthuv@gmail.com",
-        pass:"dthqdtvsagsiuivp"
+const nodemailer = require("nodemailer")
+const express = require("express")
+const sendMails = express.Router()
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "tirumalarowthuv@gmail.com",
+        pass: "dthqdtvsagsiuivp"
     }
 })
 
 
-sendMails.post("/add/send/:text",async(req,res)=>{
-    const {text}=req.params
-    const mailOptions={
-        from:"tirumalarowthuv@gmail.com",
-        to: ['Technicalmanagerp2f@gmail.com', 'Hiringmanagerp2f@gmail.com', 'hrpfsemi5@gmail.com','akshata8178@gmail.com'],
-        subject:"New Applicant Added(Applicant-Tracking-System).",
-        text: `Hi, I hope this email find you well.I want to say good news that New Applicant ${text} Added Successfully....`
+sendMails.post("/add/send/:text", async (req, res) => {
+    const { text } = req.params
+    const mailOptions = {
+        from: "tirumalarowthuv@gmail.com",
+        to: ['Technicalmanagerp2f@gmail.com', 'Hiringmanagerp2f@gmail.com', 'hrpfsemi5@gmail.com', 'akshata8178@gmail.com'],
+        subject: `New applicant ${text} added(Applicant-Tracking-System).`,
+        text: `Hello there! I hope this email finds you well. I have some exciting news to share: ${text} our new applicant, has been successfully added.`
     }
-    transporter.sendMail(mailOptions,async(err,info)=>{
-            if(err){
-                res.send(err.message)
-            }else{
-                res.send("Email sent successfully"+info.response)
-            }
+    transporter.sendMail(mailOptions, async (err, info) => {
+        if (err) {
+            res.send(err.message)
+        } else {
+            res.send("Email sent successfully" + info.response)
+        }
     })
 })
-module.exports=sendMails
+sendMails.post("/change/:email/:text", async (req, res) => {
+    const { text,email } = req.params
+    const mailOptions = {
+        from :"tirumalarowthuv@gmail.com",
+        to:email,
+        subject:`Applicant ${text} status changed.`,
+        text:`Hi,You have to an interview with ${text}`
+
+    }
+    transporter.sendMail(mailOptions,async(err,info)=>{
+        if(err){
+            res.send(err.message)
+        }else{
+            res.send("Email sent successfully "+info.response)
+        }
+    })
+})
+module.exports = sendMails
