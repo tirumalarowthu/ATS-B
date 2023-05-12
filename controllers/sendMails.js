f944831175453033471dd0935a78b11c5bd678fe
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-05-12 18:30:42 +0530

    send mail if status changed

--- controllers/sendMails.js
+++ controllers/sendMails.js
@@ -1,6 +1,8 @@
 const nodemailer = require("nodemailer")
 const express = require("express")
 const sendMails = express.Router()
+const Admin = require("../models/admin")
+
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -26,21 +28,27 @@ sendMails.post("/add/send/:text", async (req, res) => {
         }
     })
 })
-sendMails.post("/change/:email/:text", async (req, res) => {
-    const { text,email } = req.params
-    const mailOptions = {
-        from :"tirumalarowthuv@gmail.com",
-        to:email,
-        subject:`Applicant ${text} status changed.`,
-        text:`Hi,You have to an interview with ${text}`
 
-    }
-    transporter.sendMail(mailOptions,async(err,info)=>{
-        if(err){
-            res.send(err.message)
-        }else{
-            res.send("Email sent successfully "+info.response)
+sendMails.post("/change/:name/:text", async (req, res) => {
+    const { name, text } = req.params
+    const data = await Admin.findOne({ name: name })
+    if (data) {
+        const mailOptions = {
+            from: "tirumalarowthuv@gmail.com",
+            to: data.email,
+            subject: `Applicant ${text} status changed.`,
+            text: `Hi,I'm writing to inform you that the status of ${text} has changed.Now, you as the owner for ${text} , so please reach out.`
         }
-    })
+        transporter.sendMail(mailOptions, async (err, info) => {
+            if (err) {
+                res.send(err.message)
+            } else {
+                res.send("Email sent successfully " + info.response)
+            }
+        })
+    } else {
+        res.status(404).send("Email not found")
+    }
+
 })
 module.exports = sendMails
