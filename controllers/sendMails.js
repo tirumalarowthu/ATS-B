5951b1b5b75bb3ae58a0e0c14307f9e4cab5bcab
tirumalarowthu <tirumalarowthuv@gmail.com>
2023-05-26 15:15:24 +0530

    send mails changed

--- controllers/sendMails.js
+++ controllers/sendMails.js
@@ -10,15 +10,19 @@ const transporter = nodemailer.createTransport({
         pass: "dthqdtvsagsiuivp"
     }
 })
-
-
+///sending mail when applicant added .
 sendMails.post("/add/send/:text", async (req, res) => {
+
     const { text } = req.params
+    const data = await Admin.find({}, { email: 1, _id: 0 })
+    const emails = await data.map(admin => {
+        return admin.email
+    })
     const mailOptions = {
-        from: "tirumalarowthuv@gmail.com",
-        to: ['Technicalmanagerp2f@gmail.com', 'Hiringmanagerp2f@gmail.com', 'hrpfsemi5@gmail.com', 'akshata8178@gmail.com'],
+        from: "ATS-App <tirumalarowthuv@gmail.com>",
+        to: emails,
         subject: `New applicant ${text} added(Applicant-Tracking-System).`,
-        text: `Hello there! I hope this email finds you well. I have some exciting news to share: ${text} our new applicant, has been successfully added.`
+        text: `Hi, I hope this email finds you well.New Applicant ${text} added successfully.`
     }
     transporter.sendMail(mailOptions, async (err, info) => {
         if (err) {
@@ -28,16 +32,16 @@ sendMails.post("/add/send/:text", async (req, res) => {
         }
     })
 })
-
+//send mail when applicant status changed.
 sendMails.post("/change/:changeby/:name/:text", async (req, res) => {
     const { name, text, changeby } = req.params
     const data = await Admin.findOne({ name: name })
     if (data) {
         const mailOptions = {
-            from: "tirumalarowthuv@gmail.com",
+            from: "ATS-App <tirumalarowthuv@gmail.com>",
             to: data.email,
-            subject: `Applicant ${text} status changed.`,
-            text:`hi,I am writing this mail to inform you that the status of ${text} has been changed by ${changeby} and also assigned you as the new owner of ${text}.So please reach out.`
+            subject: `Applicant ${text} status changed(Applicant-Tracking-System).`,
+            text: `Hi,I am writing this mail to inform you that the status of ${text} has been changed by ${changeby} and also assigned you as the new owner of ${text}.So please reach out.`
         }
         transporter.sendMail(mailOptions, async (err, info) => {
             if (err) {
@@ -51,4 +55,5 @@ sendMails.post("/change/:changeby/:name/:text", async (req, res) => {
     }
 
 })
+//to: ['Technicalmanagerp2f@gmail.com', 'Hiringmanagerp2f@gmail.com', 'hrpfsemi5@gmail.com', 'akshata8178@gmail.com'],
 module.exports = sendMails
